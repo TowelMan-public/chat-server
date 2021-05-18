9647ee08236d403d5b39e325859edbaf6d1d9bdd
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-05-18 12:24:03 +0900

    コミット漏れ

--- src/main/java/com/example/demo/exception/AlreadyJoinTalkException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class AlreadyJoinTalkException extends Exception {
-	private static final long serialVersionUID = 1L; 
-	
-	public AlreadyJoinTalkException(String message){
-		super(message);
-	}
-}
