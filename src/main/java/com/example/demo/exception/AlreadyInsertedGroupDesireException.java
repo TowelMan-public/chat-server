89fb54e1c7f0c5cc3c073dfb0a0d7d6393267bea
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-05-18 11:34:50 +0900

    コミット漏れ

---
+++ src/main/java/com/example/demo/exception/AlreadyInsertedGroupDesireException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class AlreadyInsertedGroupDesireException extends Exception{
+	private static final long serialVersionUID = 1L; 
+	
+	public AlreadyInsertedGroupDesireException(){
+		super("User is already inserted in this group!");
+	}
+}
